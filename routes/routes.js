2a4f558098995edbbb174aced4cd962793e909a5
FIDEL DE LA ROCHA <chindacuil.45@gmail.com>
2022-05-11 12:57:42 -0500

    agregando tercera ruta

--- routes/routes.js
+++ routes/routes.js
@@ -21,4 +21,13 @@ const router =app=>{
     
 });
 
+app.get('/users/:id',(request,response)=>{
+    const id = request.params.id;
+
+    pool.query('SELECT*FROM users WHERE id=?',id,(error,result)=>{
+        if(error)throw error;
+        response.send(result);
+    });
+});
+
 }
